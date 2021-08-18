dcebd4400079287f05c548efba10c8880fefa554
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-08-18 21:06:15 +0100

    nodemailer, maka why?

--- server/services/mailer.js
+++ server/services/mailer.js
@@ -1,15 +1,65 @@
-const API_KEY = process.env.MAILGUN_API_KEY;
-const DOMAIN = process.env.MAILGUN_DOMAIN;
 const ADMIN = process.env.ADMIN_MAIL;
-const mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
+const ADMIN_RECEIVER = process.env.ADMIN_INCOMING_MAIL;
+const KEY = process.env.ADMIN_KEY;
+const HOST = process.env.ADMIN_HOST;
+const PORT = process.env.ADMIN_PORT;
+const SERVICE = process.env.NODEMAILER_SERVICE;
+const nodemailer = require("nodemailer");
 
-exports.mailer = async (to, subject, text) => {
-    const data = {
-        from: `MasterCraft <${ADMIN}>`,
-        to,
-        subject,
-        text,
+exports.mailer = async (
+    mailObj = {
+        to: ADMIN_RECEIVER,
+        subject: "TEST",
+        text: "Just something to test!",
+    }) => {
+    const result = {
+        success: null,
+        error: null,
     };
-    const sendMessage = await mailgun.messages().send(data);
-    return sendMessage;
+    try {
+        const transporter = nodemailer.createTransport({
+          host: HOST,
+          port: PORT,
+          auth: {
+            user: ADMIN,
+            pass: KEY
+          },
+          tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false
+          }
+        });
+
+        const { to, subject, text } = mailObj;
+
+        const mailOptions = {
+            from: ADMIN,
+            to,
+            subject,
+            text,
+        };
+
+        const send = await transporter.sendMail(mailOptions);
+        console.warn(send);
+        result.success = send;
+        result.error = null;
+    } catch (error) {
+        result.success = null;
+        result.error = error.message;
+    }
+    console.log(result);
+    return result;
 }
+
+// sendMail();
+
+// exports.mailer = async (to, subject, text) => {
+//     const data = {
+//         from: `MasterCraft <${ADMIN}>`,
+//         to,
+//         subject,
+//         text,
+//     };
+//     const sendMessage = await mailgun.messages().send(data);
+//     return sendMessage;
+// }
