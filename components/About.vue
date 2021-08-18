fb2ae54fcc00a725cd9ec4e8da6d43eeeeeb5010
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-08-18 10:55:06 +0100

    up-to-date with FE part

---
+++ components/About.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="px-5 has-background-grey-darker" style="border-radius: 0 0 30px 30px;">
+        <figure class="image is-128x128" style="margin: -20% 30% 0 30%;">
+            <img class="is-rounded" src="~/assets/images/dp_pg.png">
+        </figure>
+        <h3 class="title is-3 mt-5 has-text-primary">
+            Boluwaji AKINSEFUNMI <br />
+            <span class="tag is-primary mb-5">Developer</span>
+        </h3>
+        <p class="subtitle has-text-grey-light">
+            <span class="has-text-white">Phone:</span><br />
+            +234 810 341 2973
+        </p>
+        <p class="subtitle has-text-grey-light">
+            <span class="has-text-white">Email:</span><br />
+            bolu.akinsefunmi@gmail.com
+        </p>
+        <p class="subtitle has-text-grey-light">
+            <span class="has-text-white">Website:</span><br />
+            https://github.com/IAmMasterCraft
+        </p>
+        <p class="subtitle has-text-grey-light">
+            <span class="has-text-white">Location:</span><br />
+            Lagos State
+        </p>
+        <h4 class="title is-4 mt-6 has-text-primary">ABOUT ME</h4>
+        <p class="subtitle has-text-grey-light pb-6">
+            I am an easy-going and personally motivated individual with the ability to work as part of
+            a team and independently on my own.
+        </p>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,31 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+    <div class="has-background-black-bis" style="margin: 0 !important;">
+        <div class="columns">
+            <div class="column">
+                <Nav />
+            </div>
+        </div>
+        <div class="columns px-4">
+            <div class="column is-4">
+                <About />
+            </div>
+            <div class="column">
+                <div class="columns">
+                    <div class="column">
+                        <Education />
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column">
+                        <Experience />
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column">
+                        <Skills />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </section>
 </template>
-
-<script>
-import Card from '~/components/Card'
-
-export default {
-  name: 'HomePage',
-
-  components: {
-    Card
-  }
-}
-</script>
