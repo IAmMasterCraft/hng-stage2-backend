8c52df72c0135f38f67617477bba445c8d8b469d
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-08-18 22:21:16 +0100

    backend done and dusted

--- server/services/mailer.js
+++ server/services/mailer.js
@@ -1,8 +1,6 @@
 const ADMIN = process.env.ADMIN_MAIL;
 const ADMIN_RECEIVER = process.env.ADMIN_INCOMING_MAIL;
 const KEY = process.env.ADMIN_KEY;
-const HOST = process.env.ADMIN_HOST;
-const PORT = process.env.ADMIN_PORT;
 const SERVICE = process.env.NODEMAILER_SERVICE;
 const nodemailer = require("nodemailer");
 
@@ -18,16 +16,11 @@ exports.mailer = async (
     };
     try {
         const transporter = nodemailer.createTransport({
-          host: HOST,
-          port: PORT,
-          auth: {
-            user: ADMIN,
-            pass: KEY
-          },
-          tls: {
-            // do not fail on invalid certs
-            rejectUnauthorized: false
-          }
+            service: SERVICE,
+            auth: {
+                user: ADMIN,
+                pass: KEY,
+            },
         });
 
         const { to, subject, text } = mailObj;
@@ -47,19 +40,5 @@ exports.mailer = async (
         result.success = null;
         result.error = error.message;
     }
-    console.log(result);
     return result;
 }
-
-// sendMail();
-
-// exports.mailer = async (to, subject, text) => {
-//     const data = {
-//         from: `MasterCraft <${ADMIN}>`,
-//         to,
-//         subject,
-//         text,
-//     };
-//     const sendMessage = await mailgun.messages().send(data);
-//     return sendMessage;
-// }
